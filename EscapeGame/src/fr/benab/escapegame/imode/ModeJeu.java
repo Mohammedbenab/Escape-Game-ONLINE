6513e0d4a4594f903d8aa268b535b3390ea8575b
Mohammed BENABDALLAH <mela.benabdallah@gmail.com>
2019-10-10 18:12:35 +0200

    delete repository

--- EscapeGame/src/fr/benab/escapegame/imode/ModeJeu.java
+++
@@ -1,6 +0,0 @@
-package fr.benab.escapegame.imode;
-
-public interface ModeJeu {
-	public abstract void modeJeu();
-		
-}
--- EscapeGame/src/fr/benab/escapegame/imode/Proposition.java
+++
@@ -1,6 +0,0 @@
-package fr.benab.escapegame.imode;
-
-public interface Proposition {
-	public abstract int combinaison(int combinaison);
-
-}
