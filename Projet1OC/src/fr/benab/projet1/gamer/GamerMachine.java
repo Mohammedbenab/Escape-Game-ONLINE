644255d9c19384d6e680aa07caae6096be20aa44
Mohammed BENABDALLAH <mela.benabdallah@gmail.com>
2019-10-14 17:34:47 +0200

    Proposition method

--- Projet1OC/src/fr/benab/projet1/gamer/GamerMachine.java
+++ Projet1OC/src/fr/benab/projet1/gamer/GamerMachine.java
@@ -1,18 +1,50 @@
 package fr.benab.projet1.gamer;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class GamerMachine implements IAttack, IDefend{
 
 
 	@Override
 	public String combiSecret() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		int[]tab = new int[4];
+		
+		for (int i = 0; i < tab.length; i++)  {  
+	    	tab[i] = (int) ((Math.random() * ( 9 - 0 )) + 0);
+	    	System.out.print(tab[i]);
+    	}
+		String combi = Arrays.toString(tab);
+		return combi;
 	}
 
 	@Override
-	public String proposition() {
-		// TODO Auto-generated method stub
-		return null;
+	public String proposition(String[] reponse) {
+		
+		String combinaison = null;
+		int[]tab = new int[4];
+		
+		if (reponse == null && combinaison == null) {
+
+			for (int i = 0; i < tab.length; i++)  {  
+		    	tab[i] = (int) ((Math.random() * ( 9 - 0 )) + 0);
+		    	System.out.print(tab[i]);
+	    	}
+		combinaison = Arrays.toString(tab);
+		
+		}else if (reponse != null) {
+			
+			for(int j = 0; j < tab.length; j++) {
+				if (reponse[j] == "+"){
+					tab[j] = (int) (Math.random() * ( 9 - tab[j] )) ;
+				}else if(reponse[j] == "-") {
+					tab[j] = (int) (Math.random() * ( tab[j] - 0 ));
+				}else tab[j] = tab[j];
+			}
+			combinaison = Arrays.toString(tab);
+		}
+		return combinaison;
 	}
 
 }
--- Projet1OC/src/fr/benab/projet1/gamer/GamerMen.java
+++ Projet1OC/src/fr/benab/projet1/gamer/GamerMen.java
@@ -5,17 +5,16 @@ import java.util.Scanner;
 public class GamerMen implements IAttack, IDefend{
 
 	@Override
-	public String proposition() {
-		
+	public String combiSecret() {
 		System.out.println("Choisir une combinaison");
 		Scanner sc = new Scanner(System.in);
 		String combinaison = sc.nextLine();
 		return combinaison;
-		
 	}
 
 	@Override
-	public String combiSecret() {
+	public String proposition(String[] reponse) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
